afacc530f5f133c93ec14611cf03110128f80657
Real <53586056+RealEawo5@users.noreply.github.com>
2026-02-12 12:12:27 +0100

    text command

--- 2026 VÅR/A3 - Praktisk programmeringsoppgave/src/main/java/edu/ntnu/idi/idatt2003/TextCommand.java
+++ 2026 VÅR/A3 - Praktisk programmeringsoppgave/src/main/java/edu/ntnu/idi/idatt2003/TextCommand.java
@@ -1,14 +1,14 @@
-package edu.ntnu.idi.idatt2003;
-
-/**
- * Interface for text commands.
- */
-public interface TextCommand {
-    /**
-     * Execute the command on the given text.
-     *
-     * @param text the text to process
-     * @return the processed text
-     */
-    String execute(String text);
-}
+package edu.ntnu.idi.idatt2003; 
+ 
+/** 
+ * Interface for text commands. 
+ */ 
+public interface TextCommand { 
+    /** 
+     * Execute the command on the given text. 
+     * 
+     * @param text the text to process 
+     * @return the processed text 
+     */ 
+    String execute(String text); 
+} 
